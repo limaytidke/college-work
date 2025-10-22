887ef6dbc2d5fe36453cda5ce3ffeeab1fbdc1c6
LIMAY <limaytidke1@gmail.com>
2025-10-22 23:16:10 +0530

    did some rearranging of the files

---
+++ C/Lab work/simple_interest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	float P,R,T;
+	printf("\nEnter the principal amount: ");
+	scanf("%f",&P);
+	printf("Enter the  rate of interest: ");
+	scanf("%f",&R);
+	printf("Enter the time in years: ");
+	scanf("%f",&T);
+	printf("\nInterest: %.3f\n",P*R*T/100);
+}
