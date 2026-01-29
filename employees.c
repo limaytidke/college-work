82d19556f5af397ee6727e481f2924a137f153cd
LIMAY <limaytidke1@gmail.com>
2026-01-29 19:29:02 +0530

    reorganised

--- employees.c
+++ employees.c
@@ -5,7 +5,7 @@
 struct employee{
 	int ID;
 	char name[20];
-	float salary;
+	float salary,HRA,DA;
 };
 
 int main(void){
@@ -17,12 +17,14 @@ int main(void){
 		scanf("%s",&emp[i].name);
 		printf("Enter employee salary: ");
 		scanf("%f",&emp[i].salary);
+		emp[i].HRA = emp[i].salary*0.2;
+		emp[i].DA = emp[i].salary*0.4;
 	}
 	printf("\n--employee details--");
 	for (int i=0;i<3;i++){
 		printf("\nEmployee ID: %d",emp[i].ID);
 		printf("\nEmployee name: %s",emp[i].name);
-		printf("\nEmployee salary: %.2f",emp[i].salary + emp[i].salary*0.2 + emp[i].salary*0.4);
+		printf("\nEmployee salary: %.2f",emp[i].salary+emp[i].HRA+emp[i].DA);
 		printf("\n----");
 	}
 }
